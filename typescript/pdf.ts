d079230f3c5c991fdcb20bfa5e94b2b5e14ad17c
Kaspar Etter <me@kasparetter.com>
2020-05-15 14:45:41 +0200

    pdf: add script to generate a PDF of an article

---
+++ typescript/pdf.ts
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import puppeteer from 'puppeteer';
+
+const directory = process.argv[2];
+
+if (!directory) {
+    console.error('Please provide the directory as an argument.');
+    process.exit();
+}
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // https://github.com/puppeteer/puppeteer/blob/v3.0.4/docs/api.md#pagegotourl-options
+    await page.goto(`http://localhost:4000/${directory}/`, { waitUntil: 'load' });
+
+    const title = await page.evaluate(() => {
+        return document.title;
+    });
+    await page.evaluate(() => {
+        // Expand all details elements.
+        $('details').attr('open', '');
+
+        // Remove the 'srcset' attribute on all images.
+        // https://stackoverflow.com/a/61807077/12917821
+        $('img[srcset]').removeAttr('srcset');
+    });
+    // Wait for higher quality images to be loaded.
+    await new Promise(resolve => setTimeout(resolve, 500));
+    const date = new Date().toISOString().substring(0, 10);
+
+    fs.mkdirSync(`${directory}/generated`, { recursive: true });
+
+    const footerTemplate = `<div style="font-size: 8px; font-family: Lato; width: 100%; margin-bottom: 2mm; text-align: center;">
+    <span style="float: left; margin-left: 9.5mm;">
+        ${title}
+    </span>
+    <span>
+        <span class="pageNumber"></span>
+        /
+        <span class="totalPages"></span>
+    </span>
+    <span style="float: right; margin-right: 9.5mm;">
+        <a href="https://ef1p.com/${directory}/" style="color: black; text-decoration: none;">ef1p.com/${directory}</a> on ${date}
+    </span>
+</div>`;
+
+    const margin = '18mm';
+
+    // https://github.com/puppeteer/puppeteer/blob/v3.0.4/docs/api.md#pagepdfoptions
+    await page.pdf({
+        path: `${directory}/generated/${date} ${title}.pdf`,
+        width: '210mm',
+        height: '297mm',
+        margin: {
+            top: '17mm',
+            right: margin,
+            bottom: '20mm',
+            left: margin,
+        },
+        displayHeaderFooter: true,
+        headerTemplate: '<span></span>',
+        footerTemplate,
+    });
+    await browser.close();
+})();
