7d306cb166e7a9e49f4c2be1fe2fe4feb380c31d
Kaspar Etter <me@kasparetter.com>
2020-05-27 11:40:23 +0200

    links: expand all details elements in development

--- typescript/scripts/links.ts
+++ typescript/scripts/links.ts
@@ -1,9 +1,12 @@
-// This script checks the current page for broken links to anchors on the same page.
 $(document).ready(() => {
+    // This script checks the current page for broken links to anchors on the same page.
     $("a[href^='#']").each(function() {
         const hash = (this as HTMLAnchorElement).hash;
         if (hash.length > 1 && !document.getElementById(hash.substring(1))) {
             console.error('The following element has a broken link:', this);
         }
     });
+
+    // Expand all details elements.
+    $('details').attr('open', '');
 });
