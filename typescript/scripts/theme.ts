76f314a2b503e044e80949fb30b93fc65f5cf36b
Kaspar Etter <me@kasparetter.com>
2020-08-05 16:37:59 +0200

    scripts: support preferred color scheme

--- typescript/scripts/theme.ts
+++ typescript/scripts/theme.ts
@@ -1,36 +1,43 @@
 import { restoreObject, storeObject } from '../utility/storage';
 
+// Declared in the HTML head.
 declare const themes: {
     dark: string;
     light: string;
 };
 
-let theme: 'dark' | 'light' = restoreObject('theme') || 'dark';
+type Theme = keyof typeof themes;
+
+const mediaQuery = window.matchMedia('(prefers-color-scheme: light)');
+let theme: Theme = restoreObject('theme') ?? (mediaQuery.matches ? 'light' : 'dark');
 document.write('<link rel="stylesheet" id="theme" href="' + themes[theme] + '"/>');
 
-const updateTheme = () => {
+const setStylesheetSource = () => {
     const themeElement = document.getElementById('theme') as HTMLLinkElement | null;
     if (themeElement) {
         themeElement.href = themes[theme];
     }
 };
 
-const updateToggler = () => {
+const setTogglerText = () => {
     const togglerElement = document.getElementById('theme-toggler-text') as HTMLSpanElement | null;
     if (togglerElement) {
         togglerElement.textContent = theme === 'dark' ? 'Light' : 'Dark';
     }
 };
 
-const togglerClickHandler = () => {
-    theme = theme === 'dark' ? 'light' : 'dark';
+const setTheme = (newTheme: Theme) => {
+    theme = newTheme;
     storeObject('theme', theme);
-    updateTheme();
-    updateToggler();
+    setStylesheetSource();
+    setTogglerText();
 };
 
+mediaQuery.addListener(event => setTheme(event.matches ? 'light' : 'dark'));
+const togglerClickHandler = () => setTheme(theme === 'dark' ? 'light' : 'dark');
+
 const contentLoadedHandler = () => {
-    updateToggler();
+    setTogglerText();
     const toggler = document.getElementById('theme-toggler');
     if (toggler) {
         toggler.onclick = togglerClickHandler;
