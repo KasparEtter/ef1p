e34cfb8845eef445e176899cd44801f183628322
Kaspar Etter <me@kasparetter.com>
2020-08-10 12:34:18 +0200

    index: update graphic and add thumbnail

--- graphics/focus.svg.ts
+++ graphics/focus.svg.ts
@@ -5,26 +5,28 @@ import { P } from '../typescript/svg/utility/point';
 import { Circle } from '../typescript/svg/elements/circle';
 import { G } from '../typescript/svg/elements/group';
 import { printSVG } from '../typescript/svg/elements/svg';
-import { bold, small } from '../typescript/svg/elements/text';
+import { bold, small, TextLine } from '../typescript/svg/elements/text';
 
 const radius = 60;
-const gap = radius * 2.5;
+const verticalGap = 34;
+const horizontalGap = radius * 2.5;
 
-const content = [
+const content: TextLine[][] = [
     ['Tabloids', 'cover', 'people'],
     ['Newspapers', 'cover', 'events'],
     ['This blog', 'covers', 'ideas'],
 ].map(row => {
-    row[0] = small(row[0]);
-    row[1] = small(row[1]);
-    row[2] = bold(row[2]);
-    return row;
+    return [
+        small(row[0]),
+        small(row[1]),
+        bold(row[2]),
+    ];
 });
 
 const colors: Color[] = ['yellow', 'orange', 'red'];
 
 printSVG(...content.map((row, index) => {
-    const circle = new Circle({ center: P(index * gap, 0), radius, color: colors[index] });
+    const circle = new Circle({ center: P(index * horizontalGap, index * verticalGap), radius, color: colors[index] });
     const text = circle.text(row);
     return G(circle, text);
 }));
