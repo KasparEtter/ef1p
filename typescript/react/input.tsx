42322e2a13d7b2a85bf2592c96922fb54415e1c4
Kaspar Etter <me@kasparetter.com>
2020-10-06 18:11:22 +0200

    react: support column layout for forms

--- typescript/react/input.tsx
+++ typescript/react/input.tsx
@@ -11,6 +11,7 @@ export interface InputProps<State extends StateWithOnlyValues> {
     noLabels?: boolean; // Default: false.
     inline?: boolean; // Default: false.
     horizontal?: boolean; // Default: false.
+    columns?: boolean; // Default: false.
     submit?: string; // Defaults to no button.
     onSubmit?: (newState: State) => any;
 }
@@ -81,152 +82,210 @@ export class RawInput<State extends StateWithOnlyValues> extends Component<Provi
 
     private readonly randomID = '-' + Math.random().toString(36).substr(2, 8);
 
+    private labelWidth = 0;
+
+    private renderEntry = (key: string) => {
+        const entry = this.props.entries[key] as DynamicEntry<ValueType, State>;
+        const input = this.props.store.state.inputs[key];
+        const error = this.props.store.state.errors[key];
+        const state = getCurrentState(this.props.store);
+        const disabled = entry.disabled ? entry.disabled(state) : false;
+        const history = inputTypesWithHistory.includes(entry.inputType);
+        return <label
+            key={key}
+            title={entry.description}
+        >
+            {
+                !this.props.noLabels &&
+                <span
+                    className={'label-text' + (entry.inputType === 'textarea' ? ' label-for-textarea' : '')}
+                    style={this.props.horizontal ? {} : { width: this.labelWidth + 'px' }}
+                >
+                    {entry.name}:
+                </span>
+            }
+            {
+                (entry.inputType === 'checkbox' || entry.inputType === 'switch') &&
+                <span className={`custom-control custom-${entry.inputType}`}>
+                    <CustomInput
+                        name={key}
+                        type="checkbox"
+                        className="custom-control-input"
+                        checked={input as boolean}
+                        disabled={disabled}
+                        onChange={this.onChange}
+                    />
+                    <span className="custom-control-label"></span>
+                </span>
+            }
+            {
+                entry.inputType === 'select' &&
+                <select
+                    name={key}
+                    className="custom-select"
+                    disabled={disabled}
+                    onChange={this.onChange}
+                >
+                    {entry.selectOptions && Object.entries(normalizeToValue(entry.selectOptions, state)).map(
+                        ([key, text]) => <option value={key} selected={key === input}>{text}</option>,
+                    )}
+                </select>
+            }
+            {
+                entry.inputType === 'textarea' &&
+                <span className="d-inline-block">
+                    <CustomTextarea
+                        name={key}
+                        className={'form-control' + (error ? ' is-invalid' : '')}
+                        autoComplete="off"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        placeholder={entry.placeholder}
+                        disabled={disabled}
+                        onChange={this.onChange}
+                        onInput={this.onInput}
+                        value={input as string}
+                        rows={1}
+                        style={entry.inputWidth ? { width: entry.inputWidth + 'px' } : {}}
+                    />
+                    {
+                        error &&
+                        <div className="invalid-feedback">
+                            {error}
+                        </div>
+                    }
+                </span>
+            }
+            { // inputType: 'number' | 'range' | 'text' | 'password' | 'date' | 'color';
+                entry.inputType !== 'checkbox' && entry.inputType !== 'switch' && entry.inputType !== 'select' && entry.inputType !== 'textarea' &&
+                <span className="d-inline-block">
+                    <CustomInput
+                        name={key}
+                        type={entry.inputType}
+                        autoComplete="off"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        className={(entry.inputType === 'range' ? 'custom-range' : (entry.inputType === 'color' ? 'custom-color' : 'form-control')) + (error ? ' is-invalid' : '')}
+                        value={input as string | number}
+                        min={entry.minValue as string | number | undefined}
+                        max={entry.maxValue as string | number | undefined}
+                        step={entry.stepValue as string | number | undefined}
+                        placeholder={entry.placeholder}
+                        disabled={disabled}
+                        onChange={this.onChange}
+                        onInput={this.onInput}
+                        onEnter={this.onEnter}
+                        list={history ? key + this.randomID : undefined}
+                        style={entry.inputWidth ? { width: entry.inputWidth + 'px' } : {}}
+                    />
+                    {
+                        history &&
+                        <datalist id={key + this.randomID}>
+                            {normalizeToValue(entry.suggestedValues ?? [], state).concat(
+                                this.props.store.state.states.map(object => object[key]),
+                            ).reverse().filter(
+                                (option, index, self) => option !== input && self.indexOf(option) === index,
+                            ).map(
+                                option => <option value={'' + option}/>,
+                            )}
+                        </datalist>
+                    }
+                    {
+                        error &&
+                        <div className="invalid-feedback">
+                            {error}
+                        </div>
+                    }
+                </span>
+            }
+            {
+                entry.inputType === 'range' &&
+                <span className="range-value">{input}</span>
+            }
+            {
+                entry.determine &&
+                <button name={key} type="button" className="btn btn-primary btn-sm ml-2" onClick={this.onDetermine} title="Determine a suitable value.">Determine</button>
+            }
+        </label>;
+    };
+
+    private renderSubmitButton = () => {
+        const errors = !Object.values(this.props.store.state.errors).every(error => !error);
+        return this.props.submit && (this.props.store.meta.onChange || this.props.onSubmit) &&
+            <button
+                type="button"
+                className="label btn btn-sm btn-primary"
+                onClick={this.onSubmit}
+                disabled={errors}
+                title={errors ? 'Make sure that there are no errors.' : 'Submit the input fields.'}
+            >{this.props.submit}</button>;
+    };
+
+    private renderHistoryButtons = () => {
+        return !this.props.noHistory &&
+            <div
+                className="label btn-icon btn-group btn-group-sm"
+                role="group"
+                aria-label="Walk through the history of values."
+            >
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={this.onPrevious}
+                    disabled={this.props.store.state.index === 0}
+                    title="Go back to the previous set of values."
+                >
+                    <i className="fas fa-undo-alt"></i>
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={this.onClear}
+                    disabled={this.props.store.state.states.length === 1}
+                    title="Erase the history of entered values."
+                >
+                    <i className="fas fa-trash-alt"></i>
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={this.onNext}
+                    disabled={this.props.store.state.index === this.props.store.state.states.length - 1}
+                    title="Advance to the next set of values."
+                >
+                    <i className="fas fa-redo-alt"></i>
+                </button>
+            </div>;
+    };
+
     public render(): JSX.Element {
         const entries = this.props.entries;
-        const state = getCurrentState(this.props.store);
-        const maxLabelWidth = Object.values(entries).reduce((width, entry) => Math.max(width, entry!.labelWidth), 0);
-        const errors = !Object.values(this.props.store.state.errors).every(error => !error);
-        return <div className={
-            (this.props.inline ? 'inline-form' : 'block-form') + ' ' +
-            (this.props.horizontal ? 'horizontal-form' : 'vertical-form')
-        }>
-            {(Object.keys(entries)).map(key => {
-                const entry = entries[key] as DynamicEntry<ValueType, State>;
-                const input = this.props.store.state.inputs[key];
-                const error = this.props.store.state.errors[key];
-                const disabled = entry.disabled ? entry.disabled(state) : false;
-                const history = inputTypesWithHistory.includes(entry.inputType);
-                return <label
-                    key={key}
-                    title={entry.description}
-                >
-                    {
-                        !this.props.noLabels &&
-                        <span
-                            className={'label-text' + (entry.inputType === 'textarea' ? ' label-for-textarea' : '')}
-                            style={this.props.horizontal ? {} : { width: maxLabelWidth + 'px' }}
-                        >
-                            {entry.name}:
-                        </span>
-                    }
-                    {
-                        (entry.inputType === 'checkbox' || entry.inputType === 'switch') &&
-                        <span className={`custom-control custom-${entry.inputType}`}>
-                            <CustomInput
-                                name={key}
-                                type="checkbox"
-                                className="custom-control-input"
-                                checked={input as boolean}
-                                disabled={disabled}
-                                onChange={this.onChange}
-                            />
-                            <span className="custom-control-label"></span>
-                        </span>
-                    }
-                    {
-                        entry.inputType === 'select' &&
-                        <select
-                            name={key}
-                            className="custom-select"
-                            disabled={disabled}
-                            onChange={this.onChange}
-                        >
-                            {entry.selectOptions && Object.entries(normalizeToValue(entry.selectOptions, state)).map(
-                                ([key, text]) => <option value={key} selected={key === input}>{text}</option>,
-                            )}
-                        </select>
-                    }
-                    {
-                        entry.inputType === 'textarea' &&
-                        <span className="d-inline-block">
-                            <CustomTextarea
-                                name={key}
-                                className={'form-control' + (error ? ' is-invalid' : '')}
-                                autoComplete="off"
-                                autoCorrect="off"
-                                autoCapitalize="off"
-                                spellCheck="false"
-                                placeholder={entry.placeholder}
-                                disabled={disabled}
-                                onChange={this.onChange}
-                                onInput={this.onInput}
-                                value={input as string}
-                                rows={6}
-                                style={entry.inputWidth ? { width: entry.inputWidth + 'px' } : {}}
-                            />
-                            {
-                                error &&
-                                <div className="invalid-feedback">
-                                    {error}
-                                </div>
-                            }
-                        </span>
-                    }
-                    { // inputType: 'number' | 'range' | 'text' | 'password' | 'date' | 'color';
-                        entry.inputType !== 'checkbox' && entry.inputType !== 'switch' && entry.inputType !== 'select' && entry.inputType !== 'textarea' &&
-                        <span className="d-inline-block">
-                            <CustomInput
-                                name={key}
-                                type={entry.inputType}
-                                autoComplete="off"
-                                autoCorrect="off"
-                                autoCapitalize="off"
-                                spellCheck="false"
-                                className={(entry.inputType === 'range' ? 'custom-range' : (entry.inputType === 'color' ? 'custom-color' : 'form-control')) + (error ? ' is-invalid' : '')}
-                                value={input as string | number}
-                                min={entry.minValue as string | number | undefined}
-                                max={entry.maxValue as string | number | undefined}
-                                step={entry.stepValue as string | number | undefined}
-                                placeholder={entry.placeholder}
-                                disabled={disabled}
-                                onChange={this.onChange}
-                                onInput={this.onInput}
-                                onEnter={this.onEnter}
-                                list={history ? key + this.randomID : undefined}
-                                style={entry.inputWidth ? { width: entry.inputWidth + 'px' } : {}}
-                            />
-                            {
-                                history &&
-                                <datalist id={key + this.randomID}>
-                                    {normalizeToValue(entry.suggestedValues ?? [], state).concat(
-                                        this.props.store.state.states.map(object => object[key]),
-                                    ).reverse().filter(
-                                        (option, index, self) => option !== input && self.indexOf(option) === index,
-                                    ).map(
-                                        option => <option value={'' + option}/>,
-                                    )}
-                                </datalist>
-                            }
-                            {
-                                error &&
-                                <div className="invalid-feedback">
-                                    {error}
-                                </div>
-                            }
-                        </span>
-                    }
-                    {
-                        entry.inputType === 'range' &&
-                        <span className="range-value">{input}</span>
-                    }
-                    {
-                        entry.determine &&
-                        <button name={key} type="button" className="btn btn-primary btn-sm ml-2" onClick={this.onDetermine} title="Determine a suitable value.">Determine</button>
-                    }
-                </label>;
-            })}
-            {
-                this.props.submit && (this.props.store.meta.onChange || this.props.onSubmit) &&
-                <button type="button" className="label btn btn-sm btn-primary" onClick={this.onSubmit} disabled={errors} title={errors ? 'Make sure that there are no errors.' : 'Submit the input fields.'}>{this.props.submit}</button>
-            }
-            {
-                !this.props.noHistory &&
-                <div className="label btn-icon btn-group btn-group-sm" role="group" aria-label="Walk through the history of values.">
-                    <button type="button" className="btn btn-primary" onClick={this.onPrevious} disabled={this.props.store.state.index === 0} title="Go back to the previous set of values."><i className="fas fa-undo-alt"></i></button>
-                    <button type="button" className="btn btn-primary" onClick={this.onClear} disabled={this.props.store.state.states.length === 1} title="Erase the history of entered values."><i className="fas fa-trash-alt"></i></button>
-                    <button type="button" className="btn btn-primary" onClick={this.onNext} disabled={this.props.store.state.index === this.props.store.state.states.length - 1} title="Advance to the next set of values."><i className="fas fa-redo-alt"></i></button>
+        const keys = Object.keys(entries);
+        this.labelWidth = Object.values(entries).reduce((width, entry) => Math.max(width, entry!.labelWidth), 0);
+        if (this.props.columns) {
+            const numberOfEntries = keys.length + (this.props.submit ? 1 : 0) + (this.props.noHistory ? 0 : 1);
+            const startIndexOfSecondColumn = Math.ceil(numberOfEntries / 2);
+            return <div className="block-form vertical-form row">
+                <div className="col-md">
+                    {keys.slice(0, startIndexOfSecondColumn).map(this.renderEntry)}
                 </div>
-            }
-        </div>;
+                <div className="col-md">
+                    {keys.slice(startIndexOfSecondColumn).map(this.renderEntry)}
+                    {this.renderSubmitButton()}
+                    {this.renderHistoryButtons()}
+                </div>
+            </div>;
+        } else {
+            return <div className={
+                (this.props.inline ? 'inline-form' : 'block-form') + ' ' +
+                (this.props.horizontal ? 'horizontal-form' : 'vertical-form')
+            }>
+                {keys.map(this.renderEntry)}
+                {this.renderSubmitButton()}
+                {this.renderHistoryButtons()}
+            </div>;
+        }
     }
 }
