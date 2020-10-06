ab39c1fa9b56cdbd3b731bfbd89dd7ea0042a22f
Kaspar Etter <me@kasparetter.com>
2020-10-06 15:13:38 +0200

    react: add animation effect as property to click to copy element

--- typescript/react/copy.tsx
+++ typescript/react/copy.tsx
@@ -1,19 +1,28 @@
 import { createElement, MouseEvent } from 'react';
 
-import { copyToClipboardWithAnimation } from '../utility/animation';
+import { AnimationEffect, copyToClipboardWithAnimation } from '../utility/animation';
 
 import { Children } from './utility';
 
 function handleClick(event: MouseEvent<HTMLSpanElement>) {
-    copyToClipboardWithAnimation(event.currentTarget.innerText, event.currentTarget);
+    copyToClipboardWithAnimation(
+        event.currentTarget.innerText,
+        event.currentTarget,
+        event.currentTarget.dataset.effect as AnimationEffect | undefined,
+    );
 }
 
-export function ClickToCopy({ children }: Children): JSX.Element {
+export interface ClickToCopyProps {
+    effect?: AnimationEffect;
+}
+
+export function ClickToCopy({ effect, children }: ClickToCopyProps & Children): JSX.Element {
     return <span
-            onClick={handleClick}
-            className="click-to-copy"
-            title="Click to copy."
-        >
-            {children}
-        </span>;
+        onClick={handleClick}
+        className="click-to-copy"
+        title="Click to copy."
+        data-effect={effect}
+    >
+        {children}
+    </span>;
 }
