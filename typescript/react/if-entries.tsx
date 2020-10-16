8d31cb0ac1fa7d9d5527d7d7b25c16668736ba49
Kaspar Etter <me@kasparetter.com>
2020-10-16 18:15:34 +0200

    react: add inverting property to conditional entries component

--- typescript/react/if-entries.tsx
+++ typescript/react/if-entries.tsx
@@ -6,9 +6,13 @@ import { AllEntries, getCurrentState, PersistedState, ProvidedDynamicEntries, St
 import { ProvidedStore } from './share';
 import { Children } from './utility';
 
-export function RawIfEntries<State extends StateWithOnlyValues>(props: Readonly<ProvidedStore<PersistedState<State>, AllEntries<State>> & ProvidedDynamicEntries<State> & Children>): JSX.Element {
+export interface IfEntriesProps {
+    not?: boolean;
+}
+
+export function RawIfEntries<State extends StateWithOnlyValues>(props: Readonly<ProvidedStore<PersistedState<State>, AllEntries<State>> & ProvidedDynamicEntries<State> & IfEntriesProps & Children>): JSX.Element {
     const currentState = getCurrentState(props.store);
     return <Fragment>
-        {(Object.keys(props.entries) as KeysOf<State>).every(key => !!currentState[key]) && props.children}
+        {(Object.keys(props.entries) as KeysOf<State>).every(key => currentState[key]) === !props.not && props.children}
     </Fragment>;
 }
