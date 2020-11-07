d184a339753625c4b10ab0f60e8c492dce9651d7
Kaspar Etter <me@kasparetter.com>
2020-11-07 10:23:11 +0100

    svg: make protocol generation more flexible

--- typescript/svg/graphics/protocol.ts
+++ typescript/svg/graphics/protocol.ts
@@ -4,41 +4,50 @@ import { doubleTextMargin, getTextHeight, strokeRadius } from '../utility/consta
 import { P, Point, zeroPoint } from '../utility/point';
 import { rotate } from '../utility/transform';
 
-import { G } from '../elements/group';
+import { G, Group } from '../elements/group';
 import { Line } from '../elements/line';
 import { Rectangle } from '../elements/rectangle';
 import { printSVG } from '../elements/svg';
-import { bold, estimateWidth, Text } from '../elements/text';
+import { bold, estimateWidth, Text, TextLine } from '../elements/text';
+
+export interface Entity {
+    text: TextLine;
+    color?: Color;
+}
 
 export interface Message {
     from: number;
     to: number;
-    text: string;
+    text: TextLine;
     color?: Color;
     latency?: number; // How many time units it takes to deliver the message. (The default is 0.)
     delay?: number; // How much subsequent messages are delayed. (The default is 0.)
     textOffset?: Point;
 }
 
+export const textHeight = 40;
+export const textOffset = 10;
+
 function timeIncrement(message: Message): number {
     return 1 + (message.latency ?? 0) + (message.delay ?? 0);
 }
 
-export function printProtocol(
-    entities: string[],
+export function generateProtocol(
+    entities: Entity[] | string[],
     messages: Message[],
     distance: number = 238,
-): void {
-    const boldEntities = entities.map(bold);
-    const size = P(estimateWidth(boldEntities), getTextHeight(1)).add(doubleTextMargin);
-    const textHeight = 40;
-    const textOffset = 10;
+): [Group[], Group[]] {
+    if (typeof entities[0] === 'string') {
+        entities = (entities as string[]).map(text => ({ text: bold(text) }));
+    }
+    entities = entities as Entity[];
+    const size = P(estimateWidth(entities.map(entity => entity.text)), getTextHeight(1)).add(doubleTextMargin);
     const overallTime = messages.reduce((time, message) => time + timeIncrement(message), 0);
-    const entityGroups = boldEntities.map((entity, index) => {
-        const rectangle = new Rectangle({ position: P(index * distance - size.x / 2, 0), size });
-        const label = rectangle.text(entity);
+    const entityGroups = entities.map((entity, index) => {
+        const rectangle = new Rectangle({ position: P(index * distance - size.x / 2, 0), size, color: entity.color });
+        const label = rectangle.text(entity.text);
         const bottom = rectangle.boundingBox().pointAt('bottom');
-        const line = new Line({ start: bottom, end: bottom.addY(overallTime * textHeight + textOffset) });
+        const line = new Line({ start: bottom, end: bottom.addY(overallTime * textHeight + textOffset), color: entity.color });
         return G(rectangle, label, line);
     });
     let time = 1;
@@ -60,5 +69,14 @@ export function printProtocol(
         });
         return G(line, text);
     });
+    return [entityGroups, messageGroups];
+}
+
+export function printProtocol(
+    entities: Entity[] | string[],
+    messages: Message[],
+    distance: number = 238,
+): void {
+    const [entityGroups, messageGroups] = generateProtocol(entities, messages, distance);
     printSVG(...entityGroups, ...messageGroups);
 }
