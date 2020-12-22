06a816fce7a6866f84ca40180b2de0745381c1f5
Kaspar Etter <me@kasparetter.com>
2020-12-22 22:18:06 +0100

    svg: add function to generate simple flowcharts

---
+++ code/svg/graphics/flowchart.ts
@@ -0,0 +1,44 @@
+import { Color } from '../../utility/color';
+
+import { P } from '../utility/point';
+
+import { VisualElement } from '../elements/element';
+import { Line } from '../elements/line';
+import { Rectangle } from '../elements/rectangle';
+import { printSVG } from '../elements/svg';
+import { estimateSizeWithMargin, TextLine } from '../elements/text';
+
+export const defaultVerticalGap = 24;
+
+export interface Step {
+    text: TextLine | TextLine[];
+    color?: Color;
+}
+
+export function generateFlowchart(
+    steps: Step[],
+    verticalGap = defaultVerticalGap,
+): VisualElement[] {
+    const elements = new Array<VisualElement>();
+    let y = 0;
+    let previousRectangle: Rectangle | undefined;
+    for (const step of steps) {
+        const size = estimateSizeWithMargin(step.text);
+        const rectangle = new Rectangle({ position: P(-size.x / 2, y), size, color: step.color });
+        elements.push(rectangle, rectangle.text(step.text));
+        if (previousRectangle !== undefined) {
+            elements.unshift(Line.connectBoxes(previousRectangle, 'bottom', rectangle, 'top'));
+        }
+        previousRectangle = rectangle;
+        y += size.y + verticalGap;
+    }
+    return elements;
+}
+
+export function printFlowchart(
+    steps: Step[],
+    verticalGap = defaultVerticalGap,
+): void {
+    const elements = generateFlowchart(steps, verticalGap);
+    printSVG(...elements);
+}
