7c2232177c01601ad9ee026b587b44d70c4d131b
Kaspar Etter <me@kasparetter.com>
2021-04-26 23:02:43 +0200

    utility: add function to download a text file

---
+++ code/utility/download.ts
@@ -0,0 +1,9 @@
+export function download(filename: string, data: string) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
