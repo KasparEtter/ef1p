f7877035b7d41ea9e723f0ee6a6896e7c1aa8b43
Kaspar Etter <me@kasparetter.com>
2021-05-04 18:36:39 +0200

    scripts: check all links for broken targets

--- code/scripts/links.ts
+++ code/scripts/links.ts
@@ -1,17 +1,29 @@
-jQuery(() => {
-    // The following code checks the current page for broken links to anchors on the same page.
-    $("a[href^='#']").each(function() {
-        const hash = (this as HTMLAnchorElement).hash;
-        if (hash.length > 1 && !document.getElementById(hash.substring(1))) {
-            console.error('The following element has a broken link:', this);
-        }
-    });
+function isValid(href: string): boolean {
+    if (href.startsWith('#')) {
+        return document.getElementById(href.substring(1)) !== null;
+    } else {
+        return href.startsWith('https://') || href.startsWith('http://') || href.startsWith('mailto:') || href.startsWith('/');
+    }
+}
 
-    // The following code counts the number of referenced RFCs in the current article.
-    const set = new Set();
-    $("a[href^='https://tools.ietf.org/html/rfc']").each(function() {
-        set.add((this as HTMLAnchorElement).pathname.substring(6));
-    });
-    console.log(`This article references ${set.size} different RFCs.`);
+jQuery(() => {
+    // Open all information boxes.
+    // $('details').attr('open', '');
+
+    // Check the current page for broken links to anchors on the same page.
+    for (const element of document.getElementsByTagName('a')) {
+        // element.href does not return the raw value.
+        const href = element.getAttribute('href');
+        if (href !== null && !isValid(href)) {
+            console.error('The following element has a broken link:', element);
+        }
+    }
+
+    // Count the number of referenced RFCs in the current article.
+    // const set = new Set();
+    // $("a[href^='https://tools.ietf.org/html/rfc']").each(function() {
+    //     set.add((this as HTMLAnchorElement).pathname.substring(6));
+    // });
+    // console.log(`This article references ${set.size} different RFCs.`);
     // console.log(Array.from(set).sort());
 });
