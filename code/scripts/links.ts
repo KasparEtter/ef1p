ad221d8a419fe0b31bf5c27dcea820ad758859ef
Kaspar Etter <me@kasparetter.com>
2021-07-12 17:09:19 +0200

    scripts: remove the jQuery wrappings, open boxes when linking into them

--- code/scripts/links.ts
+++ code/scripts/links.ts
@@ -4,6 +4,9 @@ Work: Explained from First Principles (https://ef1p.com/)
 License: CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
 */
 
+// Open all information boxes.
+// $('details').attr('open', '');
+
 function isValid(href: string): boolean {
     if (href.startsWith('#')) {
         return document.getElementById(href.substring(1)) !== null;
@@ -12,24 +15,19 @@ function isValid(href: string): boolean {
     }
 }
 
-jQuery(() => {
-    // Open all information boxes.
-    // $('details').attr('open', '');
-
-    // Check the current page for broken links to anchors on the same page.
-    for (const element of document.getElementsByTagName('a')) {
-        // element.href does not return the raw value.
-        const href = element.getAttribute('href');
-        if (href !== null && !isValid(href)) {
-            console.error('The following element has a broken link:', element);
-        }
+// Check the current page for broken links to anchors on the same page.
+for (const element of document.getElementsByTagName('a')) {
+    // element.href does not return the raw value.
+    const href = element.getAttribute('href');
+    if (href !== null && !isValid(href)) {
+        console.error('The following element has a broken link:', element);
     }
+}
 
-    // Count the number of referenced RFCs in the current article.
-    // const set = new Set();
-    // $("a[href^='https://datatracker.ietf.org/doc/html/rfc']").each(function() {
-    //     set.add((this as HTMLAnchorElement).pathname.substring(10));
-    // });
-    // console.log(`This article references ${set.size} different RFCs.`);
-    // console.log(Array.from(set).sort());
-});
+// Count the number of referenced RFCs in the current article.
+// const set = new Set();
+// $("a[href^='https://datatracker.ietf.org/doc/html/rfc']").each(function() {
+//     set.add((this as HTMLAnchorElement).pathname.substring(10));
+// });
+// console.log(`This article references ${set.size} different RFCs.`);
+// console.log(Array.from(set).sort());
--- code/scripts/ready.ts
+++ code/scripts/ready.ts
@@ -18,253 +18,256 @@ function sanitize(hash: string): string {
     return hash;
 }
 
-jQuery(() => {
-    /* Animated scrolling to anchor with updating window title and browser history. */
+/* Animated scrolling to anchor with updating window title and browser history. */
 
-    const originalTitle = document.title;
+const originalTitle = document.title;
 
-    const getTitle = (hashOrElement: string | HTMLElement) => {
-        return originalTitle + ' at ' + $(hashOrElement as any).text();
-    };
+const getTitle = (hashOrElement: string | HTMLElement) => {
+    return originalTitle + ' at ' + $(hashOrElement as any).text();
+};
 
-    // Scrolling inspired by http://jsfiddle.net/ianclark001/rkocah23/.
-    const scrollToAnchor = (hash: string | null, trigger: 'load' | 'hash' | 'link' | 'jump') => {
-        if (!hash || !/^#[^ ]+$/.test(hash)) {
-            return false;
-        }
-
-        const anchor = trigger === 'load' ? sanitize(hash) : hash;
-        const url = window.location.pathname + anchor;
-        const target = document.getElementById(anchor.slice(1));
-        if (!target) {
-            if (trigger === 'load') {
-                report('Not found', { Type: 'Anchor', Anchor: anchor });
-            }
-            return false;
-        }
-        if (trigger === 'load') {
-            report('Load target', { Anchor: anchor });
-        }
-
-        if (target.tagName === 'SUMMARY') {
-            (target.parentElement as HTMLDetailsElement).open = true;
-        }
-
-        const offset = $(target).offset();
-        if (!offset) {
-            return false;
-        }
-
-        if (trigger !== 'load' && window.history && window.history.pushState) {
-            document.title = getTitle(anchor);
-            window.history.pushState(null, document.title, url);
-        }
-
-        $('html, body').animate({ scrollTop: offset.top - 75 });
-
-        return true;
-    };
-    scrollToAnchor(window.location.hash, 'load');
-
-    const handleHashChange = (event: JQuery.Event) => {
-        if (scrollToAnchor(window.location.hash, 'hash')) {
-            event.preventDefault();
-        }
-    };
-    $(window).on('hashchange', handleHashChange);
-
-    const handleLinkClick = (event: JQuery.TriggeredEvent) => {
-        const target = event.target.closest('a') as HTMLAnchorElement;
-        const href = target.getAttribute('href');
-        if (href === null) {
-            return;
-        }
-        if (target.classList.contains('anchorjs-link')) {
-            event.preventDefault();
-            const address = window.location.origin + window.location.pathname + href;
-            if (copyToClipboardWithAnimation(address, target, 'scale400')) {
-                report('Copy link', { Anchor: href });
-            }
-        } else if (scrollToAnchor(href, 'link')) {
-            event.preventDefault();
-        } else {
-            target.setAttribute('target', '_blank');
-        }
-    };
-    $('body').on('click', 'a', handleLinkClick);
-
-    if (window.history && window.history.replaceState) {
-        interface Heading {
-            offset: number;
-            element: HTMLElement;
-        }
-
-        let headings: Heading[];
-
-        const refreshOffsets = () => {
-            headings = [];
-            $('h2, h3, h4, h5, h6, summary').each((_, element) => {
-                const offset = $(element).offset();
-                if (offset) {
-                    headings.push({ offset: offset.top - 85, element });
-                }
-            });
-        };
-
-        let bodyHeight: number = 0;
-        let currentHeading: HTMLElement | undefined;
-
-        const handleWindowScroll = () => {
-            if (bodyHeight !== document.body.scrollHeight) {
-                bodyHeight = document.body.scrollHeight;
-                refreshOffsets();
-            }
-            for (let i = headings.length - 1; i >= 0; i--) {
-                const heading = headings[i];
-                if (window.pageYOffset > heading.offset) {
-                    if (currentHeading !== heading.element) {
-                        currentHeading = heading.element;
-                        document.title = getTitle(heading.element);
-                        window.history.replaceState(null, document.title, '#' + heading.element.id);
-                    }
-                    return;
-                }
-            }
-            if (location.hash) {
-                currentHeading = undefined;
-                document.title = originalTitle;
-                window.history.replaceState(null, document.title, location.pathname);
-            }
-        };
-
-        $(window).on('scroll', handleWindowScroll);
+// Scrolling inspired by http://jsfiddle.net/ianclark001/rkocah23/.
+const scrollToAnchor = (hash: string | null, trigger: 'load' | 'hash' | 'link' | 'jump') => {
+    if (!hash || !/^#[^ ]+$/.test(hash)) {
+        return false;
     }
 
-    /* Toggling the table of contents on small screens. */
-
-    const hideTocIfShown = () => {
-        const toc = $('#toc');
-        if (toc.hasClass('shown')) {
-            toc.removeClass('shown');
+    const anchor = trigger === 'load' ? sanitize(hash) : hash;
+    const url = window.location.pathname + anchor;
+    const target = document.getElementById(anchor.slice(1));
+    if (!target) {
+        if (trigger === 'load') {
+            report('Not found', { Type: 'Anchor', Anchor: anchor });
         }
-    };
+        return false;
+    }
+    if (trigger === 'load') {
+        report('Load target', { Anchor: anchor });
+    }
 
-    $('#toc a').on('click', hideTocIfShown);
-    $('#toc-toggler').on('click', () => $('#toc').toggleClass('shown'));
-
-    /* Jumping to next heading when clicking one. */
-
-    const jumpToNextHeading = (event: JQuery.TriggeredEvent) => {
-        if (event.target.tagName === 'SPAN') {
-            const target = event.target.closest('h2, h3, h4, h5, h6') as HTMLHeadingElement;
-            const level = parseInt(target.tagName.charAt(1), 10);
-            let element = target.nextElementSibling;
-            while (element !== null) {
-                if (['H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(element.tagName)) {
-                    if (parseInt(element.tagName.charAt(1), 10) <= level) {
-                        scrollToAnchor('#' + element.id, 'jump');
-                        break;
-                    }
-                }
-                element = element.nextElementSibling;
-            }
+    if (target.tagName === 'SUMMARY') {
+        (target.parentElement as HTMLDetailsElement).open = true;
+    } else {
+        const details = target.closest('details');
+        if (details !== null && !details.open) {
+            details.open = true;
         }
-    };
+    }
 
-    // Register the click handler only on the text instead of the whole heading by wrapping it.
-    // Please note that this has to be done before adding the anchors to get the right contents.
-    $('h2, h3, h4, h5, h6').contents().wrap('<span/>').parent().on('click', jumpToNextHeading);
-    $('summary').contents().wrap('<span/>'); // Only needed for the margin to the anchor link.
+    const offset = $(target).offset();
+    if (!offset) {
+        return false;
+    }
 
-    // Track opening and closing of information boxes.
-    $('summary').on('click', event => {
-        const summary = event.target.closest('summary');
-        if (summary !== null) {
-            const details = summary.closest('details');
-            if (details !== null && !details.open) {
-                report('Open box', { Anchor: '#' + summary.id });
-            }
-        }
-    });
+    if (trigger !== 'load' && window.history && window.history.pushState) {
+        document.title = getTitle(anchor);
+        window.history.pushState(null, document.title, url);
+    }
 
-    // Add the anchors with AnchorJS. As no IDs need to be added, this instruction can be ignored:
-    // https://www.bryanbraun.com/anchorjs/#dont-run-it-too-late
-    anchors.options = {
-        visible: 'touch',
-        titleText: 'Click to copy the link to this section.',
-    };
-    anchors.add('h2, h3, h4, h5, h6, summary');
-    $('a.anchorjs-link').attr('tabindex', -1);
+    $('html, body').animate({ scrollTop: offset.top - 75 });
 
-    // Allow the reader to download embedded SVG figures.
-    const downloadAsPNG = $('<a>').addClass(['dropdown-item']).attr('download', '').html('<i class="icon-left fas fa-file-image"></i>Download as a pixel image (PNG)');
-    const downloadAsSVG = $('<a>').addClass(['dropdown-item']).attr('download', '').html('<i class="icon-left fas fa-file-code"></i>Download as a vector graphic (SVG)');
-    const downloadMenu = $('<div>').addClass(['dropdown-menu']).append(downloadAsPNG, downloadAsSVG).appendTo('body');
+    return true;
+};
+scrollToAnchor(window.location.hash, 'load');
 
-    function showDownloadMenu(this: any, event: JQuery.MouseEventBase) {
-        const page = window.location.pathname.replace(/\//g, '') || 'index';
-        const name = $(this).data('name');
-        downloadAsPNG.attr('href', `/pages/${page}/generated/${name}.png`);
-        downloadAsSVG.attr('href', `/pages/${page}/generated/${name}.svg`);
-        downloadMenu.css({ left: event.pageX, top: event.pageY }).show();
+const handleHashChange = (event: JQuery.Event) => {
+    if (scrollToAnchor(window.location.hash, 'hash')) {
         event.preventDefault();
     }
+};
+$(window).on('hashchange', handleHashChange);
 
-    const hideDownloadMenu = () => {
-        downloadMenu.hide();
+const handleLinkClick = (event: JQuery.TriggeredEvent) => {
+    const target = event.target.closest('a') as HTMLAnchorElement;
+    const href = target.getAttribute('href');
+    if (href === null) {
+        return;
     }
-
-    // https://github.com/bryanbraun/anchorjs/blob/e084f4c8c70e620cbd65290c279c6c55ed6233eb/anchor.js#L51-L53
-    // @ts-ignore
-    const isTouchDevice = !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
-
-    if (isTouchDevice) {
-        $('figure svg.figure').on('dblclick', showDownloadMenu);
-    } else {
-        $('figure svg.figure').on('contextmenu', showDownloadMenu);
-    }
-
-    $(document).on('click', hideDownloadMenu);
-
-    // Prevent the following elements from becoming focused when clicked.
-    $('a, button, summary').on('click', function() { $(this).trigger('blur'); });
-
-    // Support abbreviations on touch devices.
-    if (isTouchDevice) {
-        $('abbr').on('click', function() { alert($(this).text() + ': ' + $(this).attr('title')); });
-    }
-
-    // Expand all information boxes.
-    $('#details-expander').on('click', _ => {
-        $('details').attr('open', '');
-        $('#details-expander').addClass('d-none');
-        $('#details-collapser').removeClass('d-none');
-        report('Open box', { Anchor: 'all' });
-    });
-
-    // Collapse all information boxes.
-    $('#details-collapser').on('click', _ => {
-        $('details').removeAttr('open');
-        $('#details-collapser').addClass('d-none');
-        $('#details-expander').removeClass('d-none');
-    });
-
-    // Track the number of PDF downloads.
-    $('#pdf-download').on('click contextmenu', event => {
-        const target = event.target as HTMLAnchorElement;
-        report('Download article', { Version: target.href });
-    });
-
-    // Remove the cookies set by earlier versions of this website.
-    if (document.cookie !== '') {
-        function clearCookie(name: string, domain = true): void {
-            document.cookie = name + '=; path=/; Secure; expires=Thu, 01 Jan 1970 00:00:00 GMT' + (domain ? '; domain=explained-from-first-principles.com' : '');
+    if (target.classList.contains('anchorjs-link')) {
+        event.preventDefault();
+        const address = window.location.origin + window.location.pathname + href;
+        if (copyToClipboardWithAnimation(address, target, 'scale400')) {
+            report('Copy link', { Anchor: href });
         }
-        clearCookie('_ga');
-        clearCookie('_gid');
-        clearCookie('_gat_gtag_UA_135908849_1');
-        clearCookie('cookieconsent_status', false);
+    } else if (scrollToAnchor(href, 'link')) {
+        event.preventDefault();
+    } else {
+        target.setAttribute('target', '_blank');
+    }
+};
+$('body').on('click', 'a', handleLinkClick);
+
+if (window.history && window.history.replaceState) {
+    interface Heading {
+        offset: number;
+        element: HTMLElement;
     }
 
-    console.log('Hi there, are you curious about how this website works? You find all the code at https://github.com/KasparEtter/ef1p. If you have any questions, just ask.');
+    let headings: Heading[];
+
+    const refreshOffsets = () => {
+        headings = [];
+        $('h2, h3, h4, h5, h6, summary').each((_, element) => {
+            const offset = $(element).offset();
+            if (offset) {
+                headings.push({ offset: offset.top - 85, element });
+            }
+        });
+    };
+
+    let bodyHeight: number = 0;
+    let currentHeading: HTMLElement | undefined;
+
+    const handleWindowScroll = () => {
+        if (bodyHeight !== document.body.scrollHeight) {
+            bodyHeight = document.body.scrollHeight;
+            refreshOffsets();
+        }
+        for (let i = headings.length - 1; i >= 0; i--) {
+            const heading = headings[i];
+            if (window.pageYOffset > heading.offset) {
+                if (currentHeading !== heading.element) {
+                    currentHeading = heading.element;
+                    document.title = getTitle(heading.element);
+                    window.history.replaceState(null, document.title, '#' + heading.element.id);
+                }
+                return;
+            }
+        }
+        if (location.hash) {
+            currentHeading = undefined;
+            document.title = originalTitle;
+            window.history.replaceState(null, document.title, location.pathname);
+        }
+    };
+
+    $(window).on('scroll', handleWindowScroll);
+}
+
+/* Toggling the table of contents on small screens. */
+
+const hideTocIfShown = () => {
+    const toc = $('#toc');
+    if (toc.hasClass('shown')) {
+        toc.removeClass('shown');
+    }
+};
+
+jQuery(() => $('#toc a').on('click', hideTocIfShown));
+$('#toc-toggler').on('click', () => $('#toc').toggleClass('shown'));
+
+/* Jumping to next heading when clicking one. */
+
+const jumpToNextHeading = (event: JQuery.TriggeredEvent) => {
+    if (event.target.tagName === 'SPAN') {
+        const target = event.target.closest('h2, h3, h4, h5, h6') as HTMLHeadingElement;
+        const level = parseInt(target.tagName.charAt(1), 10);
+        let element = target.nextElementSibling;
+        while (element !== null) {
+            if (['H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(element.tagName)) {
+                if (parseInt(element.tagName.charAt(1), 10) <= level) {
+                    scrollToAnchor('#' + element.id, 'jump');
+                    break;
+                }
+            }
+            element = element.nextElementSibling;
+        }
+    }
+};
+
+// Register the click handler only on the text instead of the whole heading by wrapping it.
+// Please note that this has to be done before adding the anchors to get the right contents.
+$('h2, h3, h4, h5, h6').contents().wrap('<span/>').parent().on('click', jumpToNextHeading);
+$('summary').contents().wrap('<span/>'); // Only needed for the margin to the anchor link.
+
+// Track opening and closing of information boxes.
+$('summary').on('click', event => {
+    const summary = event.target.closest('summary');
+    if (summary !== null) {
+        const details = summary.closest('details');
+        if (details !== null && !details.open) {
+            report('Open box', { Anchor: '#' + summary.id });
+        }
+    }
 });
+
+// Add the anchors with AnchorJS. As no IDs need to be added, this instruction can be ignored:
+// https://www.bryanbraun.com/anchorjs/#dont-run-it-too-late
+anchors.options = {
+    visible: 'touch',
+    titleText: 'Click to copy the link to this section.',
+};
+anchors.add('h2, h3, h4, h5, h6, summary');
+$('a.anchorjs-link').attr('tabindex', -1);
+
+// Allow the reader to download embedded SVG figures.
+const downloadAsPNG = $('<a>').addClass('dropdown-item').attr('download', '').html('<i class="icon-left fas fa-file-image"></i>Download as a pixel image (PNG)');
+const downloadAsSVG = $('<a>').addClass('dropdown-item').attr('download', '').html('<i class="icon-left fas fa-file-code"></i>Download as a vector graphic (SVG)');
+const downloadMenu = $('<div>').addClass('dropdown-menu').append(downloadAsPNG, downloadAsSVG).appendTo('body');
+
+function showDownloadMenu(this: any, event: JQuery.MouseEventBase) {
+    const page = window.location.pathname.replace(/\//g, '') || 'index';
+    const name = $(this).data('name');
+    downloadAsPNG.attr('href', `/pages/${page}/generated/${name}.png`);
+    downloadAsSVG.attr('href', `/pages/${page}/generated/${name}.svg`);
+    downloadMenu.css({ left: event.pageX, top: event.pageY }).show();
+    event.preventDefault();
+}
+
+const hideDownloadMenu = () => {
+    downloadMenu.hide();
+}
+
+// https://github.com/bryanbraun/anchorjs/blob/e084f4c8c70e620cbd65290c279c6c55ed6233eb/anchor.js#L51-L53
+// @ts-ignore
+const isTouchDevice = !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+
+if (isTouchDevice) {
+    $('figure svg.figure').on('dblclick', showDownloadMenu);
+} else {
+    $('figure svg.figure').on('contextmenu', showDownloadMenu);
+}
+
+$(document).on('click', hideDownloadMenu);
+
+// Prevent the following elements from becoming focused when clicked.
+$('a, button, summary').on('click', function() { $(this).trigger('blur'); });
+
+// Support abbreviations on touch devices.
+if (isTouchDevice) {
+    $('abbr').on('click', function() { alert($(this).text() + ': ' + $(this).attr('title')); });
+}
+
+// Expand all information boxes.
+$('#details-expander').on('click', _ => {
+    $('details').attr('open', '');
+    $('#details-expander').addClass('d-none');
+    $('#details-collapser').removeClass('d-none');
+    report('Open box', { Anchor: 'all' });
+});
+
+// Collapse all information boxes.
+$('#details-collapser').on('click', _ => {
+    $('details').removeAttr('open');
+    $('#details-collapser').addClass('d-none');
+    $('#details-expander').removeClass('d-none');
+});
+
+// Track the number of PDF downloads.
+$('#pdf-download').on('click contextmenu', event => {
+    const target = event.target as HTMLAnchorElement;
+    report('Download article', { Version: target.href });
+});
+
+// Remove the cookies set by earlier versions of this website.
+if (document.cookie !== '') {
+    function clearCookie(name: string, domain = true): void {
+        document.cookie = name + '=; path=/; Secure; expires=Thu, 01 Jan 1970 00:00:00 GMT' + (domain ? '; domain=explained-from-first-principles.com' : '');
+    }
+    clearCookie('_ga');
+    clearCookie('_gid');
+    clearCookie('_gat_gtag_UA_135908849_1');
+    clearCookie('cookieconsent_status', false);
+}
+
+console.log('Hi there, are you curious about how this website works? You find all the code at https://github.com/KasparEtter/ef1p. If you have any questions, just ask.');
