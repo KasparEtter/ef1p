83d8493660952afc9d037126750276b5e09b6228
Kaspar Etter <me@kasparetter.com>
2021-07-16 18:01:06 +0200

    internet: upgrade the timestamps

--- pages/internet/graphics/network-topology-nodes.ts
+++ pages/internet/graphics/network-topology-nodes.ts
@@ -4,11 +4,12 @@ Work: Explained from First Principles (https://ef1p.com/)
 License: CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
 */
 
-import { Circle } from '../../../code/svg/elements/circle';
 import { zeroPoint } from '../../../code/svg/utility/point';
 
-import { nodeProperties, nodeRadius } from './network-topology';
+import { Circle } from '../../../code/svg/elements/circle';
+
+import { defaultDistance, nodeProperties } from './network-topology';
 
 export const amount = 5;
-const points = zeroPoint.radial(6 * nodeRadius, amount, Math.PI / amount / 2 * 3);
+const points = zeroPoint.radial(defaultDistance, amount, Math.PI / amount / 2 * 3);
 export const nodes = points.map(point => new Circle({ center: point, ...nodeProperties }));
