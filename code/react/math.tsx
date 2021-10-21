92f4789f8577a7f691c47a94e39613de2bcc8024
Kaspar Etter <me@kasparetter.com>
2021-10-21 18:14:46 +0200

    react: add a component for math expressions

---
+++ code/react/math.tsx
@@ -0,0 +1,19 @@
+/*
+Author: Kaspar Etter (https://kasparetter.com/)
+Work: Explained from First Principles (https://ef1p.com/)
+License: CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
+*/
+
+import katex from 'katex';
+import { useEffect, useRef } from 'react';
+
+export interface MathProps {
+    text: string;
+    displayMode?: boolean;
+}
+
+export function Math({ text, displayMode }: MathProps): JSX.Element {
+    const reference = useRef(null);
+    useEffect(() => katex.render(text, reference.current!, { displayMode }));
+    return <span ref={reference} />;
+}
