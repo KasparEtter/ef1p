aaceb45433b61aeef88b3a3003400aa98be9bf8f
Kaspar Etter <me@kasparetter.com>
2022-10-20 19:39:50 +0200

    styles: introduce titles for tabbed areas

--- code/utility/tabbed.ts
+++ code/utility/tabbed.ts
@@ -4,8 +4,6 @@ Work: Explained from First Principles (https://ef1p.com/)
 License: CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
 */
 
-const areas = new Array<TabbedArea>();
-
 class TabbedArea {
     readonly container: JQuery<HTMLElement>;
     readonly children: JQuery<HTMLElement>;
@@ -16,13 +14,18 @@ class TabbedArea {
         element: HTMLElement,
     ) {
         this.container = $(element);
+        this.container.children().wrap('<div/>');
         this.children = this.container.children();
         this.titles = (this.container.data('titles') as string).split(' | ');
-        this.tabs = this.titles.map(title => $('<li>').text(title).on('click', () => this.activate(title)).on('dblclick', () => activateAll(title)));
+        this.children.each((index, element) => {
+            $(element).prepend($('<div>').addClass('title').text(this.titles[index]));
+        });
+        this.tabs = this.titles.map(title => $('<li>').text(title)
+            .on('click', () => this.activate(title))
+            .on('dblclick', () => activateAll(title)));
         this.container.prepend($('<ul>').addClass('tabs').append(this.tabs));
         this.container.addClass('javascript-enabled');
         this.activate(this.container.data('default') as string || this.titles[0]);
-        areas.push(this);
     }
 
     activate(title: string): void {
@@ -31,9 +34,13 @@ class TabbedArea {
             index = this.titles.length - 1;
         }
         if (index !== -1) {
+            this.container.removeClass('show-all');
             this.children.removeClass('shown');
             this.tabs.forEach(tab => tab.removeClass('active'));
             if (index === this.titles.length - 1) {
+                if (this.titles.length > 1) {
+                    this.container.addClass('show-all');
+                }
                 this.children.addClass('shown');
             } else {
                 this.children.eq(index).addClass('shown');
@@ -42,6 +49,8 @@ class TabbedArea {
         }
     }
 }
+
+const areas = new Array<TabbedArea>();
 
 export function activateAll(title: string, scroll = true): void {
     for (const area of areas) {
@@ -54,7 +63,6 @@ export function activateAll(title: string, scroll = true): void {
 
 export function bindTabbed() {
     $('.tabbed').each(function() {
-        // tslint:disable-next-line:no-unused-expression
-        new TabbedArea(this);
+        areas.push(new TabbedArea(this));
     });
 }
